1484a13505cc8e25c2ac6a21ad33b401add4a4b9
Reed Merrill <reed@Reeds-MBP.localdomain>
2024-07-26 09:31:02 -0600

    deleted silly R keybinds

--- nvim/.config/nvim/after/plugin/r.lua
+++
@@ -1,4 +0,0 @@
--- pipe operator insertion
-vim.api.nvim_set_keymap("i", "<C-->", " <- ", { noremap = true })
--- assignment operator insertion
-vim.api.nvim_set_keymap("i", "<C-;>", " |>", { noremap = true })
